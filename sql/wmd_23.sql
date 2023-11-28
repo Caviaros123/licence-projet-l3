52b46165f0cd6e8e1e0d8a8e52966c457297075f
NealBr23 <bristolneal@gmail.com>
2023-11-29 00:05:17 +0100

    Debug BDD + Insert Table

--- sql/wmd_23.sql
+++ sql/wmd_23.sql
@@ -208,40 +208,193 @@ CREATE TABLE activites_sejours (
 --- Insert table Sejour ---
 INSERT INTO sejours (date_debut_sejour, nb_places_dispo_sejour, station_sejour, prix_sejour)
 VALUES
-    ('2023-12-01', 10, 'Station A', 500.00),
-    ('2023-11-15', 5, 'Station B', 700.50),
-    ('2024-01-05', 8, 'Station C', 450.75),
-    ('2023-12-20', 12, 'Station D', 600.25);
+    ('2023-01-01', 10, 'Station A', 500.00),
+    ('2023-02-02', 5, 'Station B', 300.50),
+    ('2024-03-03', 8, 'Station C', 150.75),
+    ('2023-04-04', 12, 'Station D', 299.99),
+    ('2023-05-05', 4, 'Station E', 138.00),
+    ('2023-06-06', 2, 'Station F', 284.00),
+    ('2023-07-07', 20, 'Station G', 123.56),
+    ('2023-08-08', 17, 'Station H', 218.40);
 
 
--- Inséret table rôles
+-- Inséret table rôles --
 INSERT INTO roles (nom_role) VALUES ('Utilisateur');
 
 INSERT INTO roles (nom_role) VALUES ('Invité');
 
 INSERT INTO roles (nom_role) VALUES ('Modérateur');
 
---Insert utilisateurs
+--Insert utilisateurs --
 INSERT INTO utilisateurs (nom, prenom, email, age, mdp_utilisateur, telephone, date_inscription, id_role)
 VALUES ('Doe', 'John', 'john.doe@example.com', 30, 'motdepasse123', '123456789', NOW(), 1);
 
--- Insérer un autre utilisateur
+-- Insérer un autre utilisateur --
 INSERT INTO utilisateurs (nom, prenom, email, age, mdp_utilisateur, telephone, date_inscription, id_role)
 VALUES ('Smith', 'Jane', 'jane.smith@example.com', 25, 'mdp456', '987654321', NOW(), 2);
 
--- Insérer un troisième utilisateur
+-- Insérer un troisième utilisateur --
 INSERT INTO utilisateurs (nom, prenom, email, age, mdp_utilisateur, telephone, date_inscription, id_role)
 VALUES ('Martin', 'Luc', 'luc.martin@example.com', 40, 'monmotdepasse', '5551234567', NOW(), 1);
 
--- Insérer un asso_carita
+-- Insérer un asso_carita --
 INSERT INTO asso_carita (nom_asso_carita, descrip_asso_carita, pays_asso_carita, adresse_asso_carita, email_asso_carita, objectif_asso_carita)
 VALUES
-    ('Caritas Internationalis', 'Confédération mondiale d\'organisations caritatives catholiques.', 'Vatican', 'Adresse Caritas', 'info@caritas.org', 'Promouvoir la solidarité et la justice sociale.'),
-    ('Médecins Sans Frontières (MSF)', 'Organisation médicale humanitaire.', 'France', 'Adresse MSF', 'info@msf.org', 'Fournir des soins médicaux d\'urgence.'),
-    ('Save the Children', 'Organisation pour l\'amélioration de la vie des enfants.', 'International', 'Adresse Save the Children', 'info@savethechildren.org', 'Protéger les droits des enfants et leur fournir des opportunités.'),
-    ('Oxfam', 'Groupe international luttant contre la pauvreté et l\'injustice sociale.', 'International', 'Adresse Oxfam', 'info@oxfam.org', 'Lutte contre la pauvreté et les inégalités.'),
-    ('CARE', 'Organisation humanitaire luttant contre la pauvreté.', 'International', 'Adresse CARE', 'info@care.org', 'Fournir une aide d\'urgence et lutter contre la pauvreté.');
+    ('Association caritative 1', 'Description Association caritative 1.', 'Pays Association caritative 1', 'Adresse Association caritative 1', 'mail@Association caritative 1', 'Objectif Association caritative 1.'),
+    ('Association caritative 2', 'Description Association caritative 2.', 'Pays Association caritative 2', 'Adresse Association caritative 2', 'mail@Association caritative 2', 'Objectif Association caritative 2.'),
+    ('Association caritative 3', 'Description Association caritative 3.', 'Pays Association caritative 3', 'Adresse Association caritative 3', 'mail@Association caritative 3', 'Objectif Association caritative 3.'),
+    ('Association caritative 4', 'Description Association caritative 4.', 'Pays Association caritative 4', 'Adresse Association caritative 4', 'mail@Association caritative 4', 'Objectif Association caritative 4.'),
+    ('Association caritative 5', 'Description Association caritative 5.', 'Pays Association caritative 5', 'Adresse Association caritative 5', 'mail@Association caritative 5', 'Objectif Association caritative 5.');
 
+-- Inserer cate_don --
+ INSERT INTO cate_don (nom_cate_don, descrip_cate_don) VALUES
+('Annuels','Ce type de donnation s effectue tout les ans');
+
+INSERT INTO cate_don (nom_cate_don, descrip_cate_don) VALUES
+('Mensuelle','Ce type de donnatin s effectue tout les mois');
+
+
+--insert Activités-- 
+
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité A1', 'Station A', 50.00);
+
+-- Pour Station B
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité B1', 'Station B', 70.00);
+
+-- Pour Station C
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité C1', 'Station C', 45.00);
+
+-- Pour Station D
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité D1', 'Station D', 80.00);
+
+-- Pour Station E
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité E1', 'Station E', 90.00);
+
+-- Pour Station F
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité F1', 'Station F', 100.00);
+
+-- Pour Station G
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité G1', 'Station G', 35.00);
+
+-- Pour Station H
+INSERT INTO activites (libelle_activite, nom_station_activite, prix_activite) VALUES
+('Activité H1', 'Station H', 125.00);
+
+--Inserer activite_sejours--
+ INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(1, 1);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(2, 2);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(3, 3);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(4, 4);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(5, 5);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(6, 6);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(7, 7);
+
+INSERT INTO activites_sejours (id_activite, id_sejour) VALUES
+(8, 8);
+
+
+--Insert cate_projets--
+
+-- Aide humanitaire d'urgence
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet1', 'Description1.');
+
+-- Éducation
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet2', 'Description2.');
+
+-- Santé
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet3', 'Description3.');
+
+-- Alimentation et agriculture
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet4', 'Description4.');
+
+-- Eau et assainissement
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet5', 'Description5')
+-- Logement
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet6', 'Categorie Projet6.');
+
+-- Développement économique
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet7', 'Categorie Projet7.');
+
+-- Droits de l'homme
+INSERT INTO cate_projets (nom_cate_proj, descrip_cate_proj) VALUES
+('Categorie Projet8', 'Categorie Projet8.');
+
+
+
+--Insert evenement
+
+    INSERT INTO evenements (nom_event, descrip_event, date_debut_event, date_fin_event, lieu_event, id_assocarita, id_imagep) VALUES
+    ('Event1', 'Description Event1', '2001-01-01', '2001-02-02', 'Lieu Event1', 1, null);
+
+    INSERT INTO evenements (nom_event, descrip_event, date_debut_event, date_fin_event, lieu_event, id_assocarita, id_imagep) VALUES
+    ('Event2', 'Description Event2', '2001-03-03', '2001-04-04', 'Lieu Event2', 2, null);
+
+    INSERT INTO evenements (nom_event, descrip_event, date_debut_event, date_fin_event, lieu_event, id_assocarita, id_imagep) VALUES
+    ('Event3', 'Description Event3', '2001-05-05', '2001-06-06', 'Lieu Event3', 3, null);
+
+    INSERT INTO evenements (nom_event, descrip_event, date_debut_event, date_fin_event, lieu_event, id_assocarita, id_imagep) VALUES
+    ('Event4', 'Description Event4', '2001-07-07', '2001-08-08', 'Lieu Event4', 4, null);
+
+    INSERT INTO evenements (nom_event, descrip_event, date_debut_event, date_fin_event, lieu_event, id_assocarita, id_imagep) VALUES
+    ('Event5', 'Description Event5', '2001-09-09', '2001-10-10', 'Lieu Event5', 5, null);
+
+
+
+
+--Insert projet_carita
+
+INSERT INTO projets_carita (titre_p_car, descrip_p_car, date_debut_p_car, date_fin_p_car, id_assocarita, id_cateproj, id_imagep) VALUES
+('Projet Carita1', 'Description Projet Carita1', '2002-01-01', '2002-02-02', 1, 1, null);
+
+INSERT INTO projets_carita (titre_p_car, descrip_p_car, date_debut_p_car, date_fin_p_car, id_assocarita, id_cateproj, id_imagep) VALUES
+('Projet Carita2', 'Description Projet Carita2', '2002-03-03', '2002-04-04', 2, 2, null);
+
+INSERT INTO projets_carita (titre_p_car, descrip_p_car, date_debut_p_car, date_fin_p_car, id_assocarita, id_cateproj, id_imagep) VALUES
+('Projet Carita3', 'Description Projet Carita3', '2002-05-05', '2002-06-06', 3, 3, null);
+
+INSERT INTO projets_carita (titre_p_car, descrip_p_car, date_debut_p_car, date_fin_p_car, id_assocarita, id_cateproj, id_imagep) VALUES
+('Projet Carita4', 'Description Projet Carita4', '2002-07-07', '2002-08-08', 4, 4, null);
+
+INSERT INTO projets_carita (titre_p_car, descrip_p_car, date_debut_p_car, date_fin_p_car, id_assocarita, id_cateproj, id_imagep) VALUES
+('Projet Carita5', 'Description Projet Carita5', '2002-09-09', '2002-10-10', 5, 5, null);
+
+
+--Insert reservations
+INSERT INTO reservations (id_sejour, id_utilisateur, date_reservation, nb_personne) VALUES
+(1, 1, '2004-01-01', 5);
+
+INSERT INTO reservations (id_sejour, id_utilisateur, date_reservation, nb_personne) VALUES
+(2, 2, '2004-02-02', 3);
+
+INSERT INTO reservations (id_sejour, id_utilisateur, date_reservation, nb_personne) VALUES
+(3, 3, '2004-02-03', 4);
 
 
 
